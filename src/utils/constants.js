82e257722ab63afeb408db8fef9022be4828f114
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-07 18:03:28 -0500

    added portfolioItems to constants.js portfolioItems is an object which contains stores portfolio item info

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,30 @@
 export const sidebarWidth = "10vh";
+
+
+
+
+
+
+///////////////////////////////////// 
+//PORTFOLIO ITEMS
+
+export const portfolioItems = {
+    "AGSFlix": {
+        title: "AGSFlix",
+        description: "Test",
+        image: "Test",
+        link: "https://agsflix.vercel.app/"
+    },
+    "Trail Tours": {
+        title: "Trail Tours",
+        description: "Test",
+        image: "Test",
+        link: "https://trail-tours.vercel.app/"
+    },
+    "Playlist": {
+        title: "Playlist",
+        description: "Test",
+        image: "Test",
+        link: "https://youtube-playlist-manager-ags.vercel.app/"
+    }
+};
