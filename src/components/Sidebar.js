b3bef2678869c15d4ffa7fc8cf9aef340ffc1bfb
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-06 20:14:00 -0500

    moved sidebar styling to index.css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,6 @@
 const Item = ({ children }) => {
     return (
-        // <div className={`flex justify-center items-center
-        // h-16 w-16 rounded-3xl text-2xl`+ styles.iconBackground + styles.iconTextStyle + styles.transition} >
-        //     {children}
-        // </div>
-
-        <div className="flex justify-center items-center
-        h-16 w-16 rounded-3xl text-2xl iconBackground iconTextStyle iconTransition">
+        <div className="sidebarItem iconBackground iconTextStyle iconTransition">
           {children}
         </div>
     );
@@ -16,10 +10,7 @@ const Item = ({ children }) => {
 const Sidebar = () => {
   return (
     <div
-      className="fixed top-4 left-4 h-1/2 w-20
-      flex flex-col justify-start items-center gap-4
-      m-0 pt-10
-      shadow-2xl bg-gray-900 rounded-3xl"
+      className="sidebar"
     >
         <Item>1</Item>
         <Item>2</Item>
