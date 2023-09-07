a9d0bb3fe5ed627ea9d8b1a8d9e912b283bdba92
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-06 19:33:04 -0500

    implemented styles.js file with tailwind styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,29 @@
-const Sidebar = () => {
+import { styles } from "styles/styles";
+
+const Item = ({ children }) => {
     return (
-      <div
-        className="fixed top-0 left-0 h-screen w-16 flex flex-col m-0
-          bg-gray-900 text-white shadow-lg"
-      >
-        <i>A</i>
-        <i>B</i>
-        <i>C</i>
-      </div>
+        <div className={`flex justify-center items-center 
+        h-16 w-16 rounded-3xl text-2xl`+ styles.iconBackground + styles.iconTextStyle + styles.transition} >
+            {children}
+        </div>
     );
-  };
-  
-  export default Sidebar;
-  
+};
+
+
+const Sidebar = () => {
+  return (
+    <div
+      className="fixed top-4 left-4 h-1/2 w-20
+      flex flex-col justify-start items-center gap-4
+      m-0 pt-10
+      bg-gray-900 text-white rounded-3xl"
+    >
+        <Item>1</Item>
+        <Item>2</Item>
+        <Item>3</Item>
+        
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/styles/styles.js
@@ -0,0 +1,11 @@
+// import this file in your main app.js file
+//there you can use the styles like this:
+// import { styles } from "styles/styles";
+//
+// <div className={styles["bg-cyan-600"]}></div>
+
+export const styles = {
+    "iconBackground": " bg-slate-600 hover:bg-slate-700 ",
+    "iconTextStyle": " text-white font-bold hover:text-slate-400 ",
+    "transition": " transition duration-300 ease-in-out ",
+}
