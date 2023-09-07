f37dc9a4a272a97d3696dc96f43fed6110fd7c83
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-06 21:18:33 -0500

    added background to portfolio item cotainer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,6 +27,7 @@ const PortfolioItemContainer = () => {
   );
 };
 //TODO: make sidebarWidth a variable, possibly controlled by useState, so that it can be changed by the user
+// maybe use emotion for variable css
 function Home() {
   return (
     <div
