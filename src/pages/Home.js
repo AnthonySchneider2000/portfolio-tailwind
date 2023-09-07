7825e01631e0420b087aca76f60dd76dee15cc67
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-06 21:01:44 -0500

    set --sidebar-width from value in constants.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,40 @@
 import Sidebar from "components/Sidebar";
 import PortfolioItem from "components/PortfolioItem";
+import { sidebarWidth } from "utils/constants";
 
 const PortfolioItemContainer = () => {
   return (
     <div className="portfolioItemContainer">
       <PortfolioItem
-          title="AGSFlix"
-          description="Test"
-          image="Test"
-          link="https://agsflix.vercel.app/"
-        />
-        <PortfolioItem
-          title="Trail Tours"
-          description="Test"
-          image="Test"
-          link="https://trail-tours.vercel.app/"
-        />
-        <PortfolioItem
-          title="Playlist"
-          description="Test"
-          image="Test"
-          link="https://youtube-playlist-manager-ags.vercel.app/"
-        />
+        title="AGSFlix"
+        description="Test"
+        image="Test"
+        link="https://agsflix.vercel.app/"
+      />
+      <PortfolioItem
+        title="Trail Tours"
+        description="Test"
+        image="Test"
+        link="https://trail-tours.vercel.app/"
+      />
+      <PortfolioItem
+        title="Playlist"
+        description="Test"
+        image="Test"
+        link="https://youtube-playlist-manager-ags.vercel.app/"
+      />
     </div>
   );
 };
 
 function Home() {
   return (
-    <div className="bg-cyan-600 h-screen w-screen flex flex-col">
+    <div
+      className="bg-cyan-600 h-screen w-screen flex flex-col"
+      style={{ "--sidebar-width": sidebarWidth }} // Use the imported variable in inline style
+    >
       <Sidebar />
-        <PortfolioItemContainer />
+      <PortfolioItemContainer />
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const sidebarWidth = "10rem";
