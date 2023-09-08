7ce7d054ea9e9c2aec562fac4c4754cc28dce022
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-07 23:28:26 -0500

    added github links to projects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,19 +13,29 @@ export const portfolioItems = {
         title: "AGSFlix",
         description: "Test",
         image: "Test",
-        link: "https://agsflix.vercel.app/"
+        link: "https://agsflix.vercel.app/",
+        github: "https://github.com/AnthonySchneider2000/AGSFlix"
     },
     "TrailTours": {
         title: "Trail Tours",
         description: "Test",
         image: "Test",
-        link: "https://trail-tours.vercel.app/"
+        link: "https://trail-tours.vercel.app/",
+        github: "https://github.com/AnthonySchneider2000/React-Material-UI-Trail-Tour-Web-App"
     },
     "Playlist": {
         title: "Playlist",
         description: "Test",
         image: "Test",
-        link: "https://youtube-playlist-manager-ags.vercel.app/"
+        link: "https://youtube-playlist-manager-ags.vercel.app/",
+        github: "https://github.com/AnthonySchneider2000/NextJS-YouTube-Playlist-Manager"
+    },
+    "Portfolio": {
+        title: "Portfolio",
+        description: "Test",
+        image: "Test",
+        link: "",
+        github: "https://github.com/AnthonySchneider2000/portfolio-tailwind"
     }
 };
 
