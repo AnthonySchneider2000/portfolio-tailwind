1ea62b74386b5a488a74b8471af235fee7262007
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-07 21:19:11 -0500

    added tooltip to the sidebar items

--- src/components/Sidebar/SidebarDesktop.js
+++ src/components/Sidebar/SidebarDesktop.js
@@ -1,21 +1,29 @@
-const Item = ({ children }) => {
-    return (
-        <div className="sidebarItem">
-          {children}
-        </div>
-    );
-};
+import HomeIcon from "@mui/icons-material/Home";
+import PhoneIcon from "@mui/icons-material/Phone";
+import MailIcon from "@mui/icons-material/Mail";
+import { Tooltip } from "@mui/material";
 
+const Item = ({ icon, text = "tooltip" }) => {
+  return (
+    <Tooltip title={<div className="text-xl">{text}</div>} placement="right" componentsProps={{
+      tooltip: {
+        sx: {
+          backgroundColor: "rgb(17 24 39)", // bg-gray-900
+          padding: "0.5rem",
+        }
+      }
+    }} >
+      <div className="dsidebarItem">{icon}</div>
+    </Tooltip>
+  );
+};
 
 const SidebarDesktop = () => {
   return (
-    <div
-      className="dsidebar"
-    >
-        <Item>1</Item>
-        <Item>2</Item>
-        <Item>3</Item>
-        
+    <div className="dsidebar">
+      <Item icon={<HomeIcon />} text="Home" />
+      <Item icon={<PhoneIcon />} text="Contact" />
+      <Item icon={<MailIcon />} text="Email" />
     </div>
   );
 };
--- src/components/Sidebar/SidebarMobile.js
+++ src/components/Sidebar/SidebarMobile.js
@@ -1,21 +1,29 @@
-const Item = ({ children }) => {
-    return (
-        <div className="sidebarItem">
-          {children}
-        </div>
-    );
-};
+import HomeIcon from "@mui/icons-material/Home";
+import PhoneIcon from "@mui/icons-material/Phone";
+import MailIcon from "@mui/icons-material/Mail";
+import { Tooltip } from "@mui/material";
 
+const Item = ({ icon, text = "tooltip" }) => {
+  return (
+    <Tooltip title={<div className="text-xl">{text}</div>} placement="bottom" componentsProps={{
+      tooltip: {
+        sx: {
+          backgroundColor: "rgb(17 24 39)", // bg-gray-900
+          padding: "0.5rem",
+        }
+      }
+    }} >
+      <div className="msidebarItem">{icon}</div>
+    </Tooltip>
+  );
+};
 
 const SidebarMobile = () => {
   return (
-    <div
-      className="msidebar"
-    >
-        <Item>1</Item>
-        <Item>2</Item>
-        <Item>3</Item>
-        
+    <div className="msidebar">
+      <Item icon={<HomeIcon />} text="Home" />
+      <Item icon={<PhoneIcon />} text="Contact" />
+      <Item icon={<MailIcon />} text="Email" />
     </div>
   );
 };
