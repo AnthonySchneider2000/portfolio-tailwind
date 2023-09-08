d02741a73ebd6c144046d289d4143d76a2a70966
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-08 17:18:33 -0500

    created const components for better readability in PortfolioDetailContainer.js

--- src/components/PortfolioDetailContainer.js
+++ src/components/PortfolioDetailContainer.js
@@ -1,32 +1,39 @@
 import { useStateContext } from "utils/StateContext";
 import { portfolioItems, AGSFlix } from "utils/constants";
+import { GitHub } from "@mui/icons-material";
+
+const ProjectIsSelected = ({ project }) => {
+  return (
+    <div className="flex flex-col">
+      <Title>{project.title}</Title>
+      <div className="text-[1.2rem]">{project.description}</div>
+        <a href={project.link} target="_blank" rel="noreferrer">Link: {project.link}</a>
+        <a href={project.github} target="_blank" rel="noreferrer"><GitHub /> Source Code: {project.github}</a>
+    </div>
+  );
+};
+
+const ProjectIsNotSelected = () => {
+  return (
+      <Title>Select a project to view details</Title>
+  );
+};
 
 const Title = ({ children }) => (
-    <div className="text-[2rem] font-bold text-white">{children}</div>
+  <div className="text-[2rem] font-bold">{children}</div>
 );
 
 export default function PortfolioDetailContainer() {
-    const { projectSelected, selectedProjectName } = useStateContext();
-    const trimmedName = selectedProjectName.replace(/\s/g, '');
-    const project = portfolioItems[trimmedName] || AGSFlix;
-    return (
-        <div className="w-full bg-cyan-700 consistentRounding text-center mt-4 p-4">
-            {projectSelected ? (
-                <div className="portfolioDetail">
-                    <Title>
-                        {selectedProjectName}
-                    </Title>
-                    <div className="text-white text-[1.2rem]">
-                        {project.description}
-                    </div>
-                </div>
-            ) : (
-                <div className="portfolioDetail">
-                    <Title>
-                        Select a project to view details
-                    </Title>
-                </div>
-            )}
-        </div>
-    );
+  const { projectSelected, selectedProjectName } = useStateContext();
+  const trimmedName = selectedProjectName.replace(/\s/g, "");
+  const project = portfolioItems[trimmedName] || AGSFlix;
+  return (
+    <div className="w-full bg-cyan-700 consistentRounding text-center mt-4 p-4  text-white">
+      {projectSelected ? (
+        <ProjectIsSelected project={project} />
+      ) : (
+        <ProjectIsNotSelected />
+      )}
+    </div>
+  );
 }
