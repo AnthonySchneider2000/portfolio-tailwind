11f446fcd79acafce72ef8b48e8b8aeece7ebf6e
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-08 16:11:39 -0500

    restructured how home component is chosen

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,11 +3,17 @@ import DesktopHome from "./DesktopHome";
 import MobileHome from "./MobileHome";
 
 
+const HomeVersions = {
+  desktop: DesktopHome,
+  mobile: MobileHome,
+}
+
 function Home() {
   const isMobile = useMediaQuery({ query: "(max-aspect-ratio: 1/1)" });
+  const CurrentHome = HomeVersions[isMobile ? "mobile" : "desktop"];
   return (
     <>
-      {isMobile ? <MobileHome /> : <DesktopHome />}
+      <CurrentHome />
     </>
   );
 }
