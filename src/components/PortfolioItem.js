c6abbd056b584af4b9c0e76f400ff7f1e5b512df
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-08 00:11:43 -0500

    portfolio items are smaller on mobile

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { useStateContext } from "utils/StateContext";
+import { useMediaQuery } from "react-responsive";
 
 export default function PortfolioItem({ title, description, image, link }) {
   const { selectProject } = useStateContext();
 
+  const isMobile = useMediaQuery({ query: "(max-aspect-ratio: 1/1)" });
+  const itemHeight = isMobile ? "h-32" : "h-48";
+  const itemText = isMobile ? "text-[1.5rem]" : "text-[200%]";
   // function to select project
   const handleClick = () => {
     selectProject(title);
   };
   return (
-    <div className="portfolioItem" onClick={handleClick}>
+    <div className={`portfolioItem ${itemHeight}`} onClick={handleClick}>
       {/* <a
         href={link}
         target="_blank"
         rel="noopener noreferrer"
         className="portfolioItemText"
       > */}
-      <div className="portfolioItemText">
+      <div className={`portfolioItemText ${itemText}`}>
         <div> 
         {/* this div exists so the title doesnt touch the outside of the a tag, which has padding */} 
         {title}
