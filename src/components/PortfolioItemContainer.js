a52a68ccc95dd66228066638f0d4ff2b8d82430f
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-08 22:22:14 -0500

    indentation

--- src/components/PortfolioItemContainer.js
+++ src/components/PortfolioItemContainer.js
@@ -1,21 +1,20 @@
 import PortfolioItem from "components/PortfolioItem";
 import { portfolioItems } from "utils/constants";
 export const PortfolioItemContainer = () => {
-    return (
-      <div className="portfolioItemContainer">
-        {Object.keys(portfolioItems).map((key) => {
-          const { title, description, image, link } = portfolioItems[key];
-          return (
-            <PortfolioItem
-              key={key}
-              title={title}
-              description={description}
-              image={image}
-              link={link}
-            />
-          );
-        })}
-        
-      </div>
-    );
-  };
+  return (
+    <div className="portfolioItemContainer">
+      {Object.keys(portfolioItems).map((key) => {
+        const { title, description, image, link } = portfolioItems[key];
+        return (
+          <PortfolioItem
+            key={key}
+            title={title}
+            description={description}
+            image={image}
+            link={link}
+          />
+        );
+      })}
+    </div>
+  );
+};
