79bc842d08b0712bd5ee0ceaec401769cab564a2
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-08 20:39:57 -0500

    changed arrays to objects instead of strings

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,42 +3,45 @@ export const sidebarWidth = "7rem";
 /////////////////////////////////////
 //PORTFOLIO ITEMS
 
-export const portfolioItems = {
-  AGSFlix: {
-    title: "AGSFlix",
-    description: "Test",
-    image: "Test",
-    technologies: ["React", "MaterialUI", "Vercel"],
-    link: "https://agsflix.vercel.app/",
-    github: "https://github.com/AnthonySchneider2000/AGSFlix",
-  },
-  TrailTours: {
-    title: "Trail Tours",
-    description: "Test",
-    image: "Test",
-    technologies: ["React", "MaterialUI", "Vercel"],
-    link: "https://trail-tours.vercel.app/",
-    github:
-      "https://github.com/AnthonySchneider2000/React-Material-UI-Trail-Tour-Web-App",
-  },
-  Playlist: {
-    title: "Playlist",
-    description: "Test",
-    image: "Test",
-    technologies: ["NextJS", "Vercel"],
-    link: "https://youtube-playlist-manager-ags.vercel.app/",
-    github:
-      "https://github.com/AnthonySchneider2000/NextJS-YouTube-Playlist-Manager",
-  },
-  Portfolio: {
-    title: "Portfolio",
-    description: "Test",
-    image: "Test",
-    technologies: ["React", "TailwindCSS", "Vercel", "MaterialUI"],
-    link: "is.gd/AnthonySchneiderPortfolio",
-    github: "https://github.com/AnthonySchneider2000/portfolio-tailwind",
-  },
-};
+export const types = {
+    Layout: {
+        title: "Layout",
+        categories: ["Frontend"],
+    },
+    Styling: {
+        title: "Styling",
+        categories: ["Frontend"],
+    },
+    Scripting: {
+        title: "Scripting",
+        categories: ["Frontend", "Backend"],
+    },
+    Frontend: {
+        title: "Frontend",
+        categories: ["Frontend"],
+    },
+    Backend: {
+        title: "Backend",
+        categories: ["Backend"],
+    },
+    Database: {
+        title: "Database",
+        categories: ["Backend"],
+    },
+    Deployment: {
+        title: "Deployment",
+        categories: ["Deployment"],
+    },
+    VersionControl: {
+        title: "Version Control",
+        categories: ["Version Control"],
+    },
+    GameEngine: {
+        title: "Game Engine",
+        categories: ["Game Engine"],
+    },
+}
+
 
 export const technologies = {
   HTML: {
@@ -46,7 +49,7 @@ export const technologies = {
     description:
       "A markup language for creating web pages and web applications.",
     image: "Test",
-    types: ["Frontend", "Layout"],
+    types: types.Layout,
     link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
     requires: [],
   },
@@ -55,7 +58,7 @@ export const technologies = {
     description:
       "A style sheet language used for describing the presentation of a document written in a markup language such as HTML.",
     image: "Test",
-    types: ["Frontend", "Styling"],
+    types: types.Styling,
     link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     requires: [],
   },
@@ -64,7 +67,7 @@ export const technologies = {
     description:
       "A programming language that conforms to the ECMAScript specification.",
     image: "Test",
-    types: ["Frontend", "Scripting"],
+    types: types.Scripting,
     link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     requires: [],
   },
@@ -73,49 +76,49 @@ export const technologies = {
     description:
       "An open-source language which builds on JavaScript, one of the world’s most used tools, by adding static type definitions.",
     image: "Test",
-    types: ["Frontend", "Scripting"],
+    types: types.Scripting,
     link: "https://www.typescriptlang.org/",
-    requires: ["JavaScript"],
+    requires: this.JavaScript,
   },
   React: {
     title: "React",
     description: "A JavaScript library for building user interfaces",
     image: "Test",
-    types: ["Frontend", "Layout", "Scripting"],
+    types: [types.Layout, types.Scripting],
     link: "https://reactjs.org/",
-    requires: ["JavaScript", "HTML", "CSS"],
+    requires: [this.JavaScript, this.HTML, this.CSS],
   },
   Angular: {
     title: "Angular",
     description:
       "A TypeScript-based open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations.",
     image: "Test",
-    types: ["Frontend", "Layout", "Scripting"],
+    types: [types.Layout, types.Scripting],
     link: "https://angular.io/",
-    requires: ["TypeScript", "HTML", "CSS"],
+    requires: [this.TypeScript, this.HTML, this.CSS],
   },
   MaterialUI: {
     title: "Material UI",
     description: "React components for faster and easier web development.",
     image: "Test",
-    types: ["Frontend", "Styling"],
+    types: [types.Styling],
     link: "https://material-ui.com/",
-    requires: ["React"],
+    requires: [this.React],
   },
   NextJS: {
     title: "NextJS",
     description: "The React Framework for Production",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://nextjs.org/",
-    requires: ["React", "NodeJS"],
+    requires: [this.React, this.NodeJS],
   },
   NodeJS: {
     title: "NodeJS",
     description:
       "An open-source, cross-platform, back-end JavaScript runtime environment.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://nodejs.org/en/",
     requires: [],
   },
@@ -124,15 +127,15 @@ export const technologies = {
     description:
       "A utility-first CSS framework for rapidly building custom designs.",
     image: "Test",
-    types: ["Frontend", "Styling"],
+    types: [types.Styling],
     link: "https://tailwindcss.com/",
-    requires: ["CSS"],
+    requires: [this.CSS],
   },
   Vercel: {
     title: "Vercel",
     description: "A cloud platform for static sites and Serverless Functions",
     image: "Test",
-    types: ["Deployment"],
+    types: [types.Deployment],
     link: "https://vercel.com/",
     requires: [],
   },
@@ -141,16 +144,16 @@ export const technologies = {
     description:
       "A high-level Python Web framework that encourages rapid development and clean, pragmatic design.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://www.djangoproject.com/",
-    requires: ["Python"],
+    requires: [this.Python],
   },
   Python: {
     title: "Python",
     description:
       "An interpreted high-level general-purpose programming language.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://www.python.org/",
     requires: [],
   },
@@ -159,7 +162,7 @@ export const technologies = {
     description:
       "A source-available cross-platform document-oriented database program.",
     image: "Test",
-    types: ["Backend", "Database"],
+    types: [types.Database],
     link: "https://www.mongodb.com/",
     requires: [],
   },
@@ -167,7 +170,7 @@ export const technologies = {
     title: "MySQL",
     description: "An open-source relational database management system.",
     image: "Test",
-    types: ["Backend", "Database"],
+    types: [types.Database],
     link: "https://www.mysql.com/",
     requires: [],
   },
@@ -176,7 +179,7 @@ export const technologies = {
     description:
       "A relational database management system contained in a C library.",
     image: "Test",
-    types: ["Backend", "Database"],
+    types: [types.Database],
     link: "https://www.sqlite.org/index.html",
     requires: [],
   },
@@ -185,7 +188,7 @@ export const technologies = {
     description:
       "A distributed version-control system for tracking changes in source code during software development.",
     image: "Test",
-    types: ["Version Control"],
+    types: types.VersionControl,
     link: "https://git-scm.com/",
     requires: [],
   },
@@ -194,16 +197,16 @@ export const technologies = {
     description:
       "A provider of Internet hosting for software development and version control using Git.",
     image: "Test",
-    types: ["Version Control"],
+    types: types.VersionControl,
     link: "https://github.com/",
-    requires: ["Git"],
+    requires: [this.Git],
   },
   C: {
     title: "C",
     description:
       "A general-purpose, procedural computer programming language supporting structured programming.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://en.wikipedia.org/wiki/C_(programming_language)",
     requires: [],
   },
@@ -212,7 +215,7 @@ export const technologies = {
     description:
       "A general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://en.wikipedia.org/wiki/C%2B%2B",
     requires: [],
   },
@@ -221,7 +224,7 @@ export const technologies = {
     description:
       "A class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://www.java.com/en/",
     requires: [],
   },
@@ -230,7 +233,7 @@ export const technologies = {
     description:
       "A general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://docs.microsoft.com/en-us/dotnet/csharp/",
     requires: [],
   },
@@ -239,7 +242,7 @@ export const technologies = {
     description:
       "A cross-platform game engine developed by Unity Technologies, first announced and released in June 2005 at Apple Inc.'s Worldwide Developers Conference as a Mac OS X-exclusive game engine.",
     image: "Test",
-    types: ["Game Engine"],
+    types: [types.GameEngine],
     link: "https://unity.com/",
     requires: ["C#"],
   },
@@ -248,11 +251,49 @@ export const technologies = {
     description:
       "A multi-paradigm programming language designed for performance and safety, especially safe concurrency.",
     image: "Test",
-    types: ["Backend", "Scripting"],
+    types: [types.Scripting],
     link: "https://www.rust-lang.org/",
     requires: [],
   },
 };
+export const portfolioItems = {
+  AGSFlix: {
+    title: "AGSFlix",
+    description: "Test",
+    image: "Test",
+    technologies: [this.React, this.MaterialUI, this.Vercel],
+    link: "https://agsflix.vercel.app/",
+    github: "https://github.com/AnthonySchneider2000/AGSFlix",
+  },
+  TrailTours: {
+    title: "Trail Tours",
+    description: "Test",
+    image: "Test",
+    technologies: [this.React, this.MaterialUI, this.Vercel],
+    link: "https://trail-tours.vercel.app/",
+    github:
+      "https://github.com/AnthonySchneider2000/React-Material-UI-Trail-Tour-Web-App",
+  },
+  Playlist: {
+    title: "Playlist",
+    description: "Test",
+    image: "Test",
+    technologies: [this.NextJS, this.Vercel],
+    link: "https://youtube-playlist-manager-ags.vercel.app/",
+    github:
+      "https://github.com/AnthonySchneider2000/NextJS-YouTube-Playlist-Manager",
+  },
+  Portfolio: {
+    title: "Portfolio",
+    description: "Test",
+    image: "Test",
+    technologies: [this.React, this.MaterialUI, this.Vercel, this.TailwindCSS],
+    link: "is.gd/AnthonySchneiderPortfolio",
+    github: "https://github.com/AnthonySchneider2000/portfolio-tailwind",
+  },
+};
+
+
 
 export const AGSFlix = portfolioItems.AGSFlix;
 export const TrailTours = portfolioItems.TrailTours;
