e1d660e65772cabec6326fbf1756b1f4595d7a94
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-08 22:37:43 -0500

    Added label to portfolio

--- src/components/PortfolioItemContainer.js
+++ src/components/PortfolioItemContainer.js
@@ -2,19 +2,22 @@ import PortfolioItem from "components/PortfolioItem";
 import { portfolioItems } from "utils/constants";
 export const PortfolioItemContainer = () => {
   return (
-    <div className="portfolioItemContainer">
-      {Object.keys(portfolioItems).map((key) => {
-        const { title, description, image, link } = portfolioItems[key];
-        return (
-          <PortfolioItem
-            key={key}
-            title={title}
-            description={description}
-            image={image}
-            link={link}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="text-4xl font-bold ml-4 text-white">Portfolio</div>
+      <div className="portfolioItemContainer">
+        {Object.keys(portfolioItems).map((key) => {
+          const { title, description, image, link } = portfolioItems[key];
+          return (
+            <PortfolioItem
+              key={key}
+              title={title}
+              description={description}
+              image={image}
+              link={link}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
