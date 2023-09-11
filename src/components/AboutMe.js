c01bb9fa2cc979317ebe7179d18e588dac014cad
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-11 18:03:46 -0500

    added my photo to the aboutme section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import resume from "materials/resume.pdf";
+import MyPhoto from "materials/MyPhoto.png";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import { GitHub } from "@mui/icons-material";
 import { LinkedIn } from "@mui/icons-material";
@@ -11,13 +12,32 @@ const LinkComponent = ({ children, href }) => {
   );
 };
 
+const Photo = () => {
+  return (
+    <div className="max-h-32 aspect-square rounded-full overflow-hidden bg-cyan-800">
+      <img src={MyPhoto} alt="Anthony Schneider"  />
+    </div>
+  );
+};
+
+
 const Bio = () => {
   return (
-    <div className="text-xl">
+    <div className="text-xl flex-grow w-3/4">
       My name is Anthony Schneider. I am a software engineer with a passion for
       learning and creating. I have a B.S. in Computer Science from Southern
       Illinois University Edwardsville and am ambitious for any work
       opportunities in software development or information technology.
+    </div>
+  );
+};
+
+const TopSection = () => {
+  // contains photo and bio, photo is in the right, bio is in the left with flex-grow
+  return (
+    <div className="flex flex-row justify-center items-center gap-4">
+      <Bio />
+      <Photo />
     </div>
   );
 };
@@ -41,7 +61,7 @@ const Links = () => {
 const Container = () => {
   return (
     <div className="w-full p-4 bg-cyan-700 consistentRounding flex flex-col justify-center items-center gap-4 pb-4 text-center">
-      <Bio />
+      <TopSection />
       <Links />
     </div>
   );
