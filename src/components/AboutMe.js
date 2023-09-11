70d20942b219de74827c5087b537e29c993d7682
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-11 01:12:08 -0500

    added resume and bio to about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,22 @@
+import resume from "materials/resume.pdf";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
 export default function AboutMe() {
   return (
     <div className="text-white">
       <div className="text-4xl ml-4 font-bold">About me</div>
       <div className="w-full bg-cyan-700 consistentRounding text-center">
-        <div className="h-80 w-80 iconBackground consistentRounding">
-            test
+        <div className="text-xl p-4">
+          My name is Anthony Schneider. I am a software engineer with a passion
+          for learning and creating. <br />
+          I have a B.S. in Computer Science from Southern Illinois University
+          Edwardsville. <br />
         </div>
+        <a href={resume} target="_blank" rel="noreferrer">
+          <div className="text-2xl font-bold">
+            <AssignmentIcon fontSize="large" /> Resume
+          </div>
+        </a>
       </div>
     </div>
   );
