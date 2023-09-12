e1fa34a93d956bb73841bf228ed55c3cc67b02f0
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-12 05:00:16 -0500

    added description to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,22 +15,24 @@ const Container = ({ children }) => {
 export default function Contact() {
   return (
     <div className="text-white">
-      <div className="text-4xl ml-4 font-bold">About me</div>
+      <div className="text-4xl ml-4 font-bold">Contact</div>
 
       <Container>
+        Thank you for visiting my portfolio. If you are interested in
+        collaborating or have any inquiries, please feel free to get in touch
+        through the following channels:
         <div>
           <PhoneIcon /> (618) 623-1542
         </div>
         <div>
-          <EmailIcon /> tonyschneider3@gmail.com
+          <EmailIcon /> <a href="mailto:tonyschneider3@gmail.com">tonyschneider3@gmail.com</a>
         </div>
         <div>
-          <GitHubIcon /> AnthonySchneider2000
+          <GitHubIcon /> <a href="https://github.com/AnthonySchneider2000">GitHub</a>
         </div>
         <div>
-          <LinkedInIcon /> Anthony Schneider
+          <LinkedInIcon /> <a href="https://www.linkedin.com/in/anthony-schneider-854ab6249/">LinkedIn</a>
         </div>
-
       </Container>
     </div>
   );
