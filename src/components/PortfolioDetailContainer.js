bc81b52d18f8d04573490d7b753a7e3d94dc3732
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-12 19:22:56 -0500

    changed technology hover styling

--- src/components/PortfolioDetailContainer.js
+++ src/components/PortfolioDetailContainer.js
@@ -60,7 +60,7 @@ const Sections = ({ project }) => {
           return (
             <div
               key={index}
-              className={`m-2 p-2 w-24 h-24 rounded-md iconTransition iconBackground hover:scale-110`}
+              className={`m-2 p-2 w-24 h-24 rounded-md iconTransition hover:bg-cyan-800 hover:scale-110`}
             >
               {technology.icon}
             </div>
